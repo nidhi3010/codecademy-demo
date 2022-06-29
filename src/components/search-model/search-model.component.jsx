deb77f8bd912fbc30d44bc605bf0e0d66b59a093
studiomac-142 <studiomac-142@studiomac-142s-iMac.local>
2022-06-29 16:57:16 -0700

    search model

--- src/components/search-model/search-model.component.jsx
+++ src/components/search-model/search-model.component.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 
 import { BsSearch } from "react-icons/bs";
 
+import {BsQuestionCircle } from "react-icons/bs";
+
+
 import './search-model.styles.scss';
 
 const SearchModal = ({ handleChange }) => {
@@ -23,9 +26,9 @@ const SearchModal = ({ handleChange }) => {
   return (
     <div>
       {
-         modal &&
-            (<div className="search-modal">
-            <div className="overlay" onClick={toggleModal} />
+        modal &&
+        (<div className="search-modal">
+          <div className="overlay" onClick={toggleModal} />
           <div className="modal-content">
             <div className="search-bar">
               <BsSearch className="serach-icon" />
@@ -34,16 +37,33 @@ const SearchModal = ({ handleChange }) => {
                 placeholder="Search Our Catalog"
                 onChange={handleChange}
                 className="search"
-                
+
               />
 
             </div>
+            <div className="popular-search">
+               <h3>Popular Searches</h3>
+               </div>
+               <div className="options">
+                 <ul>
+                   <li><a href="#">Data Science</a></li>
+                   <li><a href="#">Python</a></li>
+                   <li><a href="#">Web Development</a></li>
+                   <li><a href="#">HTML</a></li>
+                   <li><a href="#">Java</a></li>
+                 </ul>
+               </div>
+               <div className="help-center">
+               <BsQuestionCircle/>
+               <a href="#">Help Center</a>
+               </div>
+             
           </div>
         </div>
-        
+
         )
       }
-      <BsSearch className='search_icon' onClick = {toggleModal} />
+      <BsSearch className='search_icon' onClick={toggleModal} />
     </div>
   )
 };
